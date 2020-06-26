558b5702ffbbd7a76be231eeb72def6723636b85
vahan levonyan <vahanl@vahans-MacBook-Pro.local>
2020-06-26 16:58:27 +0400

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+console.log('Client side javascript file is loaded!')
+
+fetch('http://localhost:3000/weather?address=boston').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
+
+const form = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'loading...'
+    messageTwo.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast = (lat, long, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=eff515c2f5d0abdcdea2fe9e9d1cd229&query=' + lat +',' +long
+    
+
+    request({url: url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if(body.error) {
+            callback('Invalid params provided', undefined)
+        } 
+        else {
+            callback(undefined, 
+                'It is ' + body.current.temperature + ' but it feels like ' + body.current.feelslike
+            )
+        }
+    })
+
+}
+
+module.exports = forecast
